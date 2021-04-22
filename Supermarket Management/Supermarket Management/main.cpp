4afad20a5d0f1a26d7182fbb0244d0b86a4571c9
Vikram <59924856+justVikram@users.noreply.github.com>
2021-04-22 22:02:14 +0530

    Added
    Supermarket Management file

---
+++ Supermarket Management/Supermarket Management/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Supermarket Management
+//
+//  Created by Aditya Vikram on 4/22/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
